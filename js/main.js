ebc1b75a6d07e83a7a56b82831fd71ed1950c513
Caique Castro <castro.caique@gmail.com>
2020-08-15 14:11:38 -0300

    Deleted unused files

--- js/main.js
+++
@@ -1,8 +0,0 @@
-(function(doc, undefined){
-  var menu_toggle = doc.getElementById("menu-toggle"),
-      menu        = doc.getElementById("menu");
-
-  menu_toggle.addEventListener("click", function () {
-    menu.classList.toggle("shown");
-  });
-}(document));
